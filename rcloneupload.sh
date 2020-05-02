241602e07cc23923650c1c23fcbd30dbdd78cc6f
daggergad7 <daggergad7@gmail.com>
2020-05-02 14:31:52 +0530

    okayall

---
+++ rcloneupload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# filePath=$3
+# relativePath=${filepath#./downloads/}
+topPath=./uploads/ # It will be the path of folder when it has multiple files, otherwise it will be the same as file path.
+
+LIGHT_GREEN_FONT_PREFIX="\033[1;32m"
+FONT_COLOR_SUFFIX="\033[0m"
+INFO="[${LIGHT_GREEN_FONT_PREFIX}INFO${FONT_COLOR_SUFFIX}]"
+
+echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Uploading to rclone"
+
+	rclone -v --config="rclone.conf" copy "$topPath" "DRIVE:info"
+
